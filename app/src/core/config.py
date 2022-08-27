2477605bbcbc33a821bc63a55d197d7949c6c5eb
White656 <kirillnb72@icloud.com>
2022-08-27 15:48:35 +0300

    adding base app configurations

--- app/src/core/config.py
+++ app/src/core/config.py
@@ -6,13 +6,28 @@ from pydantic import BaseSettings, Field
 from core.logger import LOGGING
 
 
+class RedisConfig(BaseSettings):
+    host: str = Field("127.0.0.1", env="REDIS_HOST")
+    port: int = Field(6379, env="REDIS_PORT")
+    minsize: int = Field(10, env="MIN_SIZE")
+    maxsize: int = Field(20, env="MAX_SIZE")
+
+
+class CeleryConfig(BaseSettings):
+    broker_url: str = Field("redis://localhost:6379", env="BROKER_URL", description="url адрес брокера")
+    result_url: str = Field("redis://localhost:6379", env="CELERY_RESULT_BACKEND",
+                            description="url адрес брокера результатов")
+
+
 class AppConfig(BaseSettings):
-    ...
+    name: str = Field("Document-Paragraph-Service", env="APP_NAME", description="имя приложения")
+    cache_time: int = Field(5 * 60, env="CACHE_TIME", description="Время жизни кеша в секундах")
+    celery_config: CeleryConfig = CeleryConfig()
+    redis_config: RedisConfig = RedisConfig()
 
 
-# Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
 
-APP_CONFIG = AppConfig()
+app_config = AppConfig()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
