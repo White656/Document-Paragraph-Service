2cc087147bdd2cd7990b76786959d36037f8387a
Horkov Kirill <74784236+White656@users.noreply.github.com>
2022-08-28 12:39:45 +0300

    Add files via upload

--- src/worker.py
+++ src/worker.py
@@ -1,8 +1,7 @@
-import time
 from celery import Celery
 
 from core.config import app_config
-# from services.ml import predictProbas
+from services.ml import predictProbas
 
 celery: Celery = Celery(__name__, broker=app_config.celery_config.broker_url,
                         backend=app_config.celery_config.backend_url)
@@ -10,5 +9,5 @@ celery: Celery = Celery(__name__, broker=app_config.celery_config.broker_url,
 
 @celery.task(name="predict_probas")
 def create_task(text):
-    # result = predictProbas(text)
-    return ""
+    result = predictProbas(text)
+    return result
